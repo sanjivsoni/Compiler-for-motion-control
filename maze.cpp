d6c00f95b50b617deea621fc5f811602ad65ed69
Sunjiv Soni <sanjiv1994@gmail.com>
2016-04-18 17:11:59 +0530

    Generate header file for output path
    Arduino cannot read text file directly.

--- maze.cpp
+++ maze.cpp
@@ -349,7 +349,7 @@ public:
             nextStep = previousStep;
         }
         
-        ofstream fout("path.txt");
+        ofstream fout("finalPath.h");
         if(fout == NULL)
         {
             printf("Cannot write Path to output file\n");
